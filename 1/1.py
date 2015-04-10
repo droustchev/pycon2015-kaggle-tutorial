fd6f4afb97249d9428552e5018f2c53c3abd3503
Dimitar Roustchev <d.roustchev@googlemail.com>
2015-04-09 22:42:37 -0400

    My adaptions during the tutorial.

--- 1/1.py
+++ 1/1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def load_feature_matrix(path):
-	df = pd.read_csv(path)
+	df = pd.read_csv(path, parse_dates=['PostCreationDate','OwnerCreationDate'])
 	df["TitleLength"] = df.Title.apply(len)
+	df['TimeToPost'] = df.PostCreationDate.astype('i8') - df.OwnerCreationDate.astype('i8')
 	return df
